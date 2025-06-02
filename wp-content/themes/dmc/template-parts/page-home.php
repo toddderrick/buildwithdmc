1d6ef5954bca975af43e9a582e8b5aad075c890e
Todd Baylis <toddbaylis.web@gmail.com>
2025-06-01 18:12:32 -0700

    Add home page template

---
+++ wp-content/themes/dmc/template-parts/page-home.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Template Name: Home
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Bootscore
+ * @version 6.0.0
+ */
+
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
+get_header();
+?>
+
+  <div id="content" class="dmc-site-content dmc-page-home site-content">
+    <div id="primary" class="content-area">
+
+      <main id="main" class="site-main">
+
+        <div class="dmc-hero">
+            <div class="dmc-video-bg">
+              <video autoplay muted loop id="myVideo">
+                <source src="https://buildwithdmc.s3.ca-central-1.amazonaws.com/videos/home-video-1.mp4" type="video/mp4">
+              </video>
+            </div>
+        </div>
+
+        <div class="entry-content">
+          <?php the_post(); ?>
+          <?php the_content(); ?>
+        </div>
+
+      </main>
+
+    </div>
+  </div>
+
+<?php
+get_footer();
